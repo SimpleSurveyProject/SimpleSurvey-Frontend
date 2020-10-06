aae747cf0a8d07c680d0b8652bfe357f49f6e74f
Patrick Braje <patrick.braje@gmail.com>
2020-10-06 12:42:07 +0200

    Angular Material added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simpleSurveyFrontend';
+  title = 'SimpleSurvey';
+
+  buttonText = "a";
+
+  onClick() {
+    if(this.buttonText == "a")this.buttonText = "b";
+    else this.buttonText = "a";
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,17 @@
-import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-
-    MatButtonModule
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
