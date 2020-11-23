813278e1f31ea6ca01a8a33ba010a08d5c1a8bc7
Patrick Braje <patrick.braje@gmail.com>
2020-11-23 13:03:55 +0100

    Quick fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,31 +2,38 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'http://direct.simplesurvey.de:1337/api/auth/';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(credentials): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
-      username: credentials.username,
-      password: credentials.password
-    }, httpOptions);
+    return this.http.post(
+      AUTH_API + 'signin',
+      {
+        username: credentials.username,
+        password: credentials.password,
+      },
+      httpOptions
+    );
   }
 
   register(user): Observable<any> {
     console.log(user);
-    return this.http.post(AUTH_API + 'signup', {
-      username: user.username,
-      password: user.password
-    }, httpOptions);
+    return this.http.post(
+      AUTH_API + 'signup',
+      {
+        username: user.username,
+        password: user.password,
+      },
+      httpOptions
+    );
   }
 }
